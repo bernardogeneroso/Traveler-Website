8cee2d44e35723bd1666da97f33c6da4f2f6eb37
Bernardo Generoso <58465456+bernardogeneroso@users.noreply.github.com>
2021-01-30 21:41:06 +0000

    Prepared front-end

---
+++ front-end/src/pages/Main/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const Main = () => {
+  return <Container>Olá</Container>;
+};
+
+export default Main;
---
+++ front-end/src/routes/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Main from "../pages/Main";
+const Routes: React.FC = () => {
+  return (
+    <Switch>
+      <Route path="/" component={Main} exact />
+    </Switch>
+  );
+};
+
+export default Routes;
